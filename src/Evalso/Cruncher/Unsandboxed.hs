f9e17743da1cd3feee55bce770b81f5445672ab2
Ricky Elrod <ricky@elrod.me>
2015-01-09 18:37:07 -0500

    Make version return a more useful type
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Evalso/Cruncher/Unsandboxed.hs
+++ src/Evalso/Cruncher/Unsandboxed.hs
@@ -4,11 +4,14 @@ module Evalso.Cruncher.Unsandboxed (version) where
 
 import Evalso.Cruncher.Language (Language, rpm)
 
-import Control.Applicative
 import Control.Lens
 import qualified Data.Text as T
 import qualified Shelly as S
 
-version :: Language -> IO T.Text
-version l = S.shelly $ S.silently $
-  T.strip <$> S.run "rpm" ["-qv", T.pack (l ^. rpm)]
+version :: Language -> IO (Maybe T.Text)
+version l = S.shelly . S.errExit False . S.silently $ do
+  versionOut <- S.run "rpm" ["-qv", T.pack (l ^. rpm)]
+  exitCode <- S.lastExitCode
+  return $ if exitCode == 0
+           then Just . T.strip $ versionOut
+           else Nothing
